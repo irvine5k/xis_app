9c5be6e552a938125bdad84206fd1438b6ac5549
Kelven <kelvengalvao@gmail.com>
2019-04-11 16:37:17 -0300

    Started project and create some screens

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import Header from './src/components/Header'
+import Post from './src/components/Post'
+
+
+export default class App extends Component {
+    render() {
+        return (
+            <View style={{ flex: 1 }}>
+                <Header />
+                <Post image={require('./assets/imgs/fence.jpg')} /> 
+            </View>
+            
+        )
+    }
+}
+
+
